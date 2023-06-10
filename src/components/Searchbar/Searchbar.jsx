52d99f72b4dfd3d81a4fe30c1332ab8ded6281b7
Darya Zaloha <dashulia.zaloga@gmail.com>
2023-06-10 16:18:11 +0300

    icon

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Search, Form, Btn, Span, Input } from './Searchbar.styled';
+import { ReactComponent as Icon } from '../iconbar.svg';
 
 class Searchbar extends Component {
   state = {
@@ -22,6 +23,7 @@ class Searchbar extends Component {
       <Search>
         <Form onSubmit={this.handleSubmit}>
           <Btn type="submit">
+            <Icon width="30px" height="30px" />
             <Span>Search</Span>
           </Btn>
 
